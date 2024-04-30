5e39bdef49046e357044bf00a1d346b384b65e23
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-04-30 20:53:19 +0800

    add endpoints for messaging

--- gallery/views.py
+++ gallery/views.py
@@ -4,10 +4,10 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-from .models import Artwork, Category, Bid
+from .models import Artwork, Category, Bid, Message, MessageThread
 from myauth.models import User
 from myauth.serializers import SimpleUserSerializer
-from .serializers import ArtworkSerializer, CategorySerializer, BidSerializer
+from .serializers import ArtworkSerializer, CategorySerializer, BidSerializer, MessageThreadSerializer, MessageSerializer
 from .permissions import IsArtworkOwner, IsBidOwner
 
 
@@ -167,3 +167,51 @@ class TopArtistsView(APIView):
 
         serializer = SimpleUserSerializer(artists, many=True)
         return Response(serializer.data)
+    
+
+class MessagesView(APIView):
+    def get(self, request):
+        queryset = MessageThread.objects.all()
+        filters = {}
+        for key in request.query_params.keys():
+            filters[key] = request.query_params[key]
+        
+        if filters:
+            queryset = queryset.filter(**filters)
+        
+        serializer = MessageThreadSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class MessageThreadView(APIView):
+    def get(self, request, pk=None):
+        if pk is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        queryset = MessageThread.objects.all()
+        message_thread = get_object_or_404(queryset, pk=pk)
+        serializer = MessageThreadSerializer(message_thread)
+        return Response(serializer.data)
+
+
+class MessageView(APIView):
+    def get(self, request, pk=None):
+        if pk is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        queryset = Message.objects.all()
+        message = get_object_or_404(queryset, pk=pk)
+        serializer = MessageSerializer(message)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def delete(self, request, pk=None):
+    #     message = Message.objects.get(pk=pk)
+    #     message.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT
