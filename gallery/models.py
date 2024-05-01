9b97fea6a86b553be6b353199c1ebd5999ad37a9
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-05-01 23:42:26 +0800

    allow artwork category to be empty

--- gallery/models.py
+++ gallery/models.py
@@ -14,7 +14,7 @@ class Artwork(models.Model):
 
     artist = models.ForeignKey(User, on_delete=models.CASCADE, related_name='artworks_created')
     buyer = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='artworks_bought')
-    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=100)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
