bf291ae8c529b40ec30b78e63d881d1788eeb371
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-05-01 22:18:17 +0800

    fix middleware bug

--- myauth/middlewares.py
+++ myauth/middlewares.py
@@ -12,6 +12,8 @@ class JWTAuthentication:
         token = request.COOKIES.get('jwt')
         if not token:
             token_header = request.headers.get('Authorization')
+            if not token_header:
+                return self.get_response(request)
             if 'Bearer ' in token_header:
                 token = token_header.split('Bearer ')[1]
 
