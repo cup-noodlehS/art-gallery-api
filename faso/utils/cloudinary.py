21c5cb66b29200f29053ddb1dbac6d5a6207e1f9
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-05-11 09:10:01 +0800

    add delete image function

--- faso/utils/cloudinary.py
+++ faso/utils/cloudinary.py
@@ -20,3 +20,18 @@ def upload_to_cloudinary(file, folder, width=300, height=300, crop='fill'):
         return avatar_urls[0]
     except Exception as e:
         raise e
+    
+
+def delete_from_cloudinary(url):
+    public_id = url.split('/')[-1].split('.')[0]
+    try:
+        result = cloudinary.uploader.destroy(public_id)
+        if result['result'] == 'ok':
+            print("Image deleted successfully")
+            return True
+        else:
+            print("Error deleting image:", result['message'])
+            raise Exception(result['message'])
+    except Exception as e:
+        print("Error deleting image:", e)
+        raise e
