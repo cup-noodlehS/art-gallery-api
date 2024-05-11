9975d42e939c442a029dcf6b0cdeba285f53e6bc
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-05-11 09:10:34 +0800

    add post delete signal for user

--- myauth/signals.py
+++ myauth/signals.py
@@ -1,5 +1,12 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from cloudinary.uploader import destroy
 
 from .models import User
+from faso.utils import delete_from_cloudinary
 
+
+@receiver(post_delete, sender=User)
+def delete_user(sender, instance, **kwargs):
+    avatar_url = instance.avatar_url
+    delete_from_cloudinary(avatar_url)
