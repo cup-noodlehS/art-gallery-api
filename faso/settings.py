5b11f04148e3e0fea44a6585bcb3903a7efae34c
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-06-10 17:09:48 +0800

    change middleware order

--- faso/settings.py
+++ faso/settings.py
@@ -51,14 +51,14 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = 'faso.asgi.application'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'myauth.middlewares.JWTAuthentication',
 ]
 
