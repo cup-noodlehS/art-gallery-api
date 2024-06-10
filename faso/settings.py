60c63724a63b36be120b2a1625f26386a2c56621
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-06-10 17:21:22 +0800

    temp allow all origins

--- faso/settings.py
+++ faso/settings.py
@@ -141,10 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'myauth.User'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://moonine.vercel.app",
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://moonine.vercel.app",
+#     "http://localhost:3000",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 CORS_ALLOW_METHODS = [
     "DELETE",
