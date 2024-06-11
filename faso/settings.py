088cb68f2532cd41525aaa7a2d00f5fc3cb108dd
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-06-11 10:31:56 +0800

    limit allowed cors origins

--- faso/settings.py
+++ faso/settings.py
@@ -141,11 +141,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'myauth.User'
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://moonine.vercel.app",
-#     "http://localhost:3000",
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://moonine.vercel.app",
+    "http://localhost:3000",
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOW_METHODS = [
